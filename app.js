4d9ac2b8ad609e7d69419bf6bc1bea7a054bce6e
Mathew-Seliverstov <71725707+Mathew-Seliverstov@users.noreply.github.com>
2021-06-16 18:07:43 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -19,6 +19,15 @@ function verticalSliderPlugin() {
 		changeSlide('down')
 	})
 
+	document.addEventListener('keydown', event => {
+		if (event.key === 'ArrowUp') {
+			changeSlide('up')
+		} else if (event.key === 'ArrowDown') {
+			changeSlide('down')
+		}
+		
+	})
+
 
 
 	function changeSlide(direction) {
